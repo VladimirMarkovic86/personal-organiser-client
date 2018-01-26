0bd3d05e42aaf5d8117834700e6e3c6e21d10195
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-01-26 20:05:04 +0100

    Login improvized

--- project.clj
+++ project.clj
@@ -1,60 +1,53 @@
 (defproject personal-organiser-client "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-	:source-paths		["src/clj"]
-	:resource-paths	["resources"]
-  :dependencies	[	[org.clojure/clojure				"1.8.0"]
-  									;	https://clojure.org/api/api
-  								[org.clojure/clojurescript	"1.9.946"]
-  									;	https://clojurescript.org/guides/quick-start#clojurescript-compiler
-  									;	https://clojurescript.org/reference/dependencies	
-  								[reagent										"0.8.0-alpha2"]
-  									;	https://github.com/reagent-project/reagent
-  								[hiccups										"0.3.0"]
-  									;	https://github.com/teropa/hiccups
-  								[enfocus										"2.1.1"]
-  									;	https://github.com/ckirkendall/enfocus
-  									;	https://github.com/levand/domina
-  								]
+  :description    "FIXME: write description"
+  :url            "http://example.com/FIXME"
+  :license        {:name "Eclipse Public License"
+                   :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths   ["src/clj"]
+  :resource-paths ["resources"]
+  :dependencies   [[org.clojure/clojure    "1.8.0"]
+                    ; https://clojure.org/api/api
+                   [org.clojure/clojurescript "1.9.946"]
+                    ; https://clojurescript.org/guides/quick-start#clojurescript-compiler
+                    ; https://clojurescript.org/reference/dependencies 
+                   [reagent          "0.8.0-alpha2"]
+                    ; https://github.com/reagent-project/reagent
+                   [hiccups          "0.3.0"]
+                    ; https://github.com/teropa/hiccups
+                   [enfocus          "2.1.1"]
+                    ; https://github.com/ckirkendall/enfocus
+                    ; https://github.com/levand/domina
+                   ]
   
-	:plugins	[	[lein-figwheel		"0.5.14"]
-								;	https://github.com/bhauman/lein-figwheel
-							[lein-cljsbuild		"1.1.7"]
-								;	https://github.com/emezeske/lein-cljsbuild
-							]
+  :plugins [[lein-figwheel  "0.5.14"]
+             ; https://github.com/bhauman/lein-figwheel
+            [lein-cljsbuild  "1.1.7"]
+             ; https://github.com/emezeske/lein-cljsbuild
+            ]
   
-	:cljsbuild
-		{	:builds
-			{	:dev
-					{	:source-paths		[	"src/cljs"]
-						:compiler				{	:main							personal-organiser-client.core
-															:output-to				"resources/public/js/main.js"
-															:output-dir				"resources/public/js/out"
-															:asset-path				"js/out"
-															:pretty-print			true
-															}
-						}
-				:figwheel
-					{	:source-paths		[	"src/cljs"]
-						:figwheel				{	:open-urls				["http://localhost:3449/index.html"]	}
-						:compiler				{	:main							personal-organiser-client.core
-															:output-to				"resources/public/jsfig/main.js"
-															:output-dir				"resources/public/jsfig/out"
-															:asset-path				"jsfig/out"
-															:pretty-print			true
-															}
-						}
-				:prod
-					{	:source-paths		[	"src/cljs"]
-						:compiler				{	:main							personal-organiser-client.core
-															:output-to				"resources/public/jsprod/main.js"
-															:output-dir				"resources/public/jsprod/out"
-															:asset-path				"jsprod/out"
-															:optimizations		:advanced
-															}
-						}
-  			}
-			}
-	)
+  :cljsbuild
+   {:builds
+    {:dev
+     {:source-paths ["src/cljs"]
+      :compiler     {:main         personal-organiser-client.core
+                     :output-to    "resources/public/js/main.js"
+                     :output-dir   "resources/public/js/out"
+                     :asset-path   "js/out"
+                     :pretty-print true}}
+     :figwheel
+     {:source-paths ["src/cljs"]
+      :figwheel     {:open-urls    ["http://localhost:3449/index.html"] }
+      :compiler     {:main         personal-organiser-client.core
+                     :output-to    "resources/public/jsfig/main.js"
+                     :output-dir   "resources/public/jsfig/out"
+                     :asset-path   "jsfig/out"
+                     :pretty-print true}}
+     :prod
+     {:source-paths ["src/cljs"]
+      :compiler     {:main          personal-organiser-client.core
+                     :output-to     "resources/public/jsprod/main.js"
+                     :output-dir    "resources/public/jsprod/out"
+                     :asset-path    "jsprod/out"
+                     :optimizations :advanced}}
+     }}
+  )
--- src/cljs/personal_organiser_client/core.cljs
+++ src/cljs/personal_organiser_client/core.cljs
@@ -1,120 +1,141 @@
 (ns personal-organiser-client.core
-	(:require	[cljsjs.react							:as	react]
-						[enfocus.core							:as	ef]
-            [enfocus.events						:as	events]
-            [personal-organiser-client.ajax	:as	ajx])
-	(:require-macros	[enfocus.macros														:as	em]
-										[personal-organiser-client.html-generator	:as	hg]))
+  (:require [cljsjs.react              :as react]
+            [enfocus.core              :as ef]
+            [enfocus.events            :as events]
+            [personal-organiser-client.ajax :as ajx])
+  (:require-macros [enfocus.macros                            :as em]
+                   [personal-organiser-client.html-generator  :as hg]))
 
-(defn	success-handler
-	"Simple ajax success handler"
-	[	xhr
-		params-map]
-	(let	[	resp			(aget	xhr	"response")
-					elements	(.getElementsByClassName	js/document	(:entity	params-map))]
-		(.log	js/console	xhr)
-		(aset	(aget	elements	0)	"innerHTML"	(str	resp	" - success"))
-		))
+(defn success-handler
+  "Simple ajax success handler"
+  [xhr
+   params-map]
+  (let [resp      (aget xhr "response")
+        elements (.getElementsByClassName js/document (:entity params-map))]
+    (.log js/console xhr)
+    (aset (aget elements 0) "innerHTML" (str resp " - success"))
+    ))
 
-(defn	error-handler
-	"Simple ajax error handler"
-	[	xhr
-		params-map]
-	(let	[	resp			(aget	xhr	"response")
-					elements	(.getElementsByClassName	js/document	(:entity	params-map))]
-		(.log	js/console	xhr)
-		(aset	(aget	elements	0)	"innerHTML"	(str	resp	" - error"))
-		))
+(defn error-handler
+  "Simple ajax error handler"
+  [xhr
+   params-map]
+  (let [resp      (aget xhr "response")
+        elements  (.getElementsByClassName js/document (:entity params-map))]
+    (.log js/console xhr)
+    (aset (aget elements 0) "innerHTML" (str resp " - error"))
+    ))
 
-(hg/deftmpl	login-form	"public/login-form.html")
-(hg/deftmpl	template	"public/template.html")
-(hg/deftmpl	nav	"public/nav.html")
-(hg/deftmpl	form	"public/form.html")
-(hg/deftmpl	footer	"public/footer.html")
+(hg/deftmpl login-form "public/login-form.html")
+(hg/deftmpl template "public/template.html")
+(hg/deftmpl nav "public/nav.html")
+(hg/deftmpl form "public/form.html")
+(hg/deftmpl footer "public/footer.html")
+
+(em/defaction remove-main
+  []
+  [".header"]
+    (ef/remove-node)
+  [".content"]
+    (ef/remove-node)
+  [".footer"]
+    (ef/remove-node))
 
 (em/defaction on-page-load
-	[]
-	["body"]
-		(ef/content	template)
-	[".header"]
-		(ef/content	nav)
-	[".content"]
-		(ef/content	form)
-	[".footer"]
-		(ef/content	footer)
-	["#testBtnId"]
-		(events/listen	:click
-										#(do	(ajx/uni-ajax-call
-														{	:url									"https://localhost:8443/clojure/index1"
-															:request-method				"POST"
-															:success-fn						success-handler
-															:error-fn							error-handler
-															:request-header-map
-																{	"Accept"				"application/json"
-																	"Content-Type"	"application/json"}
-															:request-property-map
-																{	"responseType"	"text/javascript"}
-															:entity								"index1"
-															})
-													(ajx/uni-ajax-call
-														{	:url									"https://localhost:8443/clojure/index2"
-															:request-method				"POST"
-															:success-fn						success-handler
-															:error-fn							error-handler
-															:request-header-map
-																{	"Accept"				"application/json"
-																	"Content-Type"	"application/json"}
-															:request-property-map
-																{	"responseType"	"text/javascript"}
-															:entity								"index2"
-															}))
-										))
+  []
+  ["body"]
+    (ef/prepend template)
+  [".header"]
+    (ef/content nav)
+  [".content"]
+    (ef/content form)
+  [".footer"]
+    (ef/content footer)
+  ["#aLogoutId"]
+    (events/listen :click
+                   #(do (remove-main)
+                        (is-logged-in "2"))
+     )
+  ["#testBtnId"]
+    (events/listen :click
+                    #(do (ajx/uni-ajax-call
+                           { :url                  "https://localhost:8443/clojure/index1"
+                             :request-method        "POST"
+                              :success-fn            success-handler
+                              :error-fn              error-handler
+                              :request-header-map
+                                {  "Accept"        "application/json"
+                                  "Content-Type"  "application/json"}
+                              :request-property-map
+                                {  "responseType"  "text/javascript"}
+                              :entity                "index1"
+                              })
+                          (ajx/uni-ajax-call
+                            {  :url                  "https://localhost:8443/clojure/index2"
+                              :request-method        "POST"
+                              :success-fn            success-handler
+                              :error-fn              error-handler
+                              :request-header-map
+                                {  "Accept"        "application/json"
+                                  "Content-Type"  "application/json"}
+                              :request-property-map
+                                {  "responseType"  "text/javascript"}
+                              :entity                "index2"
+                              }))
+                    ))
 
-(defn	login-success
-	"Login success"
-	[]
-	(.log	js/console	"success"))
+(em/defaction remove-login
+  []
+  ["table.login"]
+   (ef/remove-node))
 
-(defn	login-error
-	"Login error"
-	[]
-	(.log	js/console	"error"))
+(defn login-success
+  "Login success"
+  [xhr]
+  (remove-login)
+  (is-logged-in (aget xhr "response"))
+  )
 
-(defn	read-login-form
-	"Read data from login form"
-	[]
-	(let	[	email			(.getElementById	js/document	"txtEmailId")
-					password	(.getElementById	js/document	"pswLoginId")]
-		{	:email			email
-			:password		password}))
+(defn login-error
+  "Login error"
+  []
+  (.log js/console "error"))
+
+(defn read-login-form
+  "Read data from login form"
+  []
+  (let [email    (.getElementById js/document "txtEmailId")
+        password (.getElementById js/document "pswLoginId")]
+    {:email      (aget email "value")
+     :password   (aget password "value")}))
 
 (em/defaction redirect-to-login
-	[]
-	["body"]
-		(ef/content	login-form)
-	["#btnLoginId"]
-		(events/listen	:click
-										#(ajx/uni-ajax-call
-											{	:url									"https://localhost:8443/clojure/login"
-												:request-method				"POST"
-												:success-fn						login-success
-												:error-fn							login-error
-												:request-header-map
-													{	"Accept"				"application/json"
-														"Content-Type"	"application/json"}
-												:request-property-map
-													{	"responseType"	"application/json"}
-												:entity								"{}";(read-login-form)
-												}))
-	)
+  []
+  ["body"]
+    (ef/prepend login-form)
+  ["#btnLoginId"]
+    (events/listen :click
+                   #(ajx/uni-ajax-call
+                      {:url                  "https://localhost:8443/clojure/login"
+                       :request-method        "POST"
+                       :success-fn            login-success
+                       :error-fn              login-error
+                       :request-header-map
+                        {"Accept"        "application/json"
+                         "Content-Type"  "application/json"}
+                       :request-property-map
+                        {"responseType"  "application/json"}
+                       :entity                (read-login-form)
+                        }))
+  )
 
-(defn	is-logged-in
-	"Check if user is logged in"
-	[]
-	(if	(= 1 2)
-		(on-page-load)
-		(redirect-to-login))
-	)
+(defn is-logged-in
+  "Check if user is logged in"
+  [param]
+  (if (= "1" param)
+    (on-page-load)
+    (redirect-to-login))
+  )
 
-(set! (.-onload js/window)	is-logged-in)
+(set! (.-onload js/window) (is-logged-in "2"))
 
