22d03ade8bb3c46ce835398fd8a81468db25c4f3
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-01-23 17:19:21 +0100

    Task 51: Accepted language bug fixed.

--- project.clj
+++ project.clj
@@ -8,14 +8,14 @@
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.2"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.1"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.2"]
-                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.10"]
+                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.11"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.2"]
-                 [org.clojars.vladimirmarkovic86/language-lib "0.2.5"]
+                 [org.clojars.vladimirmarkovic86/language-lib "0.2.6"]
                  [org.clojars.vladimirmarkovic86/personal-organiser-middle "0.2.2"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.2"]
-                 [org.clojars.vladimirmarkovic86/common-client "0.3.6"]
+                 [org.clojars.vladimirmarkovic86/common-client "0.3.7"]
                  [org.clojars.vladimirmarkovic86/server-lib "0.3.13"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.1"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.2"]
                  ]
 
   :min-lein-version "2.0.0"
