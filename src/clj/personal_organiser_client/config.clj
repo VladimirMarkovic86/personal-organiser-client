c25300c23008c1a2c59e99763d19688b70b6f091
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-03-18 13:41:48 +0100

    Task63: Implemented tests for utils-lib and partially for server-lib and common-server

---
+++ src/clj/personal_organiser_client/config.clj
@@ -0,0 +1,36 @@
+(ns personal-organiser-client.config
+  (:require [server-lib.core :as srvr]))
+
+(defn define-port
+  "Defines server's port"
+  []
+  (let [port (System/getenv "PORT")
+        port (if port
+               (read-string
+                 port)
+               1611)]
+    port))
+
+(defn build-certificates-map
+  "Build certificates map"
+  []
+  (let [certificates {:keystore-file-path
+                       "certificate/personal_organiser_client.jks"
+                      :keystore-password
+                       "ultras12"}
+        certificates (when-not (System/getenv "CERTIFICATES")
+                       certificates)]
+    certificates))
+
+(defn set-thread-pool-size
+  "Set thread pool size"
+  []
+  (let [thread-pool-size (System/getenv "THREAD_POOL_SIZE")]
+    (when thread-pool-size
+      (reset!
+        srvr/thread-pool-size
+        (read-string
+          thread-pool-size))
+     ))
+ )
+
