ad53e12e2252563bf97613b73d053aa250d398d1
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-06-19 11:52:33 +0200

    Task 70: Created row and col css file and used them on home page.

--- src/cljs/personal_organiser_client/html.cljs
+++ src/cljs/personal_organiser_client/html.cljs
@@ -1,5 +1,5 @@
 (ns personal-organiser-client.html
-  (:require [htmlcss-lib.core :refer [h2 p]]
+  (:require [htmlcss-lib.core :refer [h2 p div]]
             [personal-organiser-middle.functionalities :as pomfns]
             [common-client.allowed-actions.controller :refer [allowed-actions]]
             [personal-organiser-client.grocery.html :as gh]
@@ -11,11 +11,29 @@
 (defn home-page-content
   "Home page content"
   []
-  [(h2
-     (get-label 62))
-   (p
-     (get-label 63))]
- )
+  [(div
+     [(h2
+        (get-label 62))
+      (p
+        (get-label 63))
+      ]
+     {:class "row-1-4"})
+   (div
+     [(div
+        nil
+        {:class "col-1-4"})
+      (div
+        nil
+        {:class "col-2-4 logo-hi-res"})
+      (div
+        nil
+        {:class "col-1-4"})
+      ]
+     {:class "row-2-4"})
+   (div
+     nil
+     {:class "row-1-4"})
+   ])
 
 (defn custom-menu
   "Render menu items for user that have privilege for them"
