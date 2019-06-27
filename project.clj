8b5b849c5e6caf190173f712b1ad1b49addf8d69
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-06-27 14:22:40 +0200

    Task 63: Prepared tests for client-test-lib.core namespace.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [org.clojars.vladimirmarkovic86/language-lib "0.2.31"]
                  [org.clojars.vladimirmarkovic86/personal-organiser-middle "0.2.10"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
-                 [org.clojars.vladimirmarkovic86/common-client "0.3.40"]
+                 [org.clojars.vladimirmarkovic86/common-client "0.3.41"]
                  [org.clojars.vladimirmarkovic86/server-lib "0.3.30"]
                  [org.clojars.vladimirmarkovic86/validator-lib "0.1.32"]
                  ]
