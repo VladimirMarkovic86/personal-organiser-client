bf6d5e8c73bfc5d0d839435b102dfa9ac5cd0765
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:17:47 +0200

    Task 88: Excluded public folder from some more clojurescripts projects packing jar file.

--- project.clj
+++ project.clj
@@ -8,14 +8,14 @@
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.12"]
                  [org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.7"]
                  [org.clojars.vladimirmarkovic86/js-lib "0.1.17"]
-                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.46"]
+                 [org.clojars.vladimirmarkovic86/framework-lib "0.2.47"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
                  [org.clojars.vladimirmarkovic86/language-lib "0.2.34"]
                  [org.clojars.vladimirmarkovic86/personal-organiser-middle "0.2.10"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.9"]
-                 [org.clojars.vladimirmarkovic86/common-client "0.3.44"]
+                 [org.clojars.vladimirmarkovic86/common-client "0.3.45"]
                  [org.clojars.vladimirmarkovic86/server-lib "0.3.33"]
-                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.35"]
+                 [org.clojars.vladimirmarkovic86/validator-lib "0.1.36"]
                  ]
 
   :min-lein-version "2.0.0"
