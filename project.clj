59b83fa650ff7ab09a7ff8920c41d15413256a3d
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-09-26 17:44:19 +0200

    Task99: Improved ocr-lib and constrained server-lib to less parallel threads support.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                  [org.clojars.vladimirmarkovic86/personal-organiser-middle "0.2.14"]
                  [org.clojars.vladimirmarkovic86/common-middle "0.2.13"]
                  [org.clojars.vladimirmarkovic86/common-client "0.3.50"]
-                 [org.clojars.vladimirmarkovic86/server-lib "0.3.35"]
+                 [org.clojars.vladimirmarkovic86/server-lib "0.3.36"]
                  [org.clojars.vladimirmarkovic86/validator-lib "0.1.41"]
                  ]
 
